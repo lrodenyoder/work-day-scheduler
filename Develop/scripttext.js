7d2f77cc2e998c8610f9ea9bd711b1dc14a553b7
lrodenyoder <rodenlau000@gmail.com>
2022-01-21 17:35:37 -0600

    localstorage start over

--- Develop/scripttext.js
+++ Develop/scripttext.js
@@ -5,6 +5,7 @@ var todayDate = moment().format("dddd LL");
 var currentTime = moment().format("H");
 //array to hold each hour of the scheduler
 var textAreaArray = $(".description");
+var eventArray = [];
 
 //display current date at the top of scheduler
 $("#currentDay").replaceWith(todayDate);
@@ -34,6 +35,53 @@ var auditTime = function () {
   }
 };
 
+var textAreaInput = $(".description").val();
+console.log(textAreaInput);
+
+var saveEvent = function () {
+  //var eventInput = $(".description").val();
+  var eventID = $(".description");
+  //console.log(eventInput);
+
+  // var eventObj = {
+  //     input: eventInput,
+  //     //dataID:
+  // }
+
+  //eventArray.push(eventObj);
+  //console.log(textAreaArray[0].innerHTML);
+  for (var i = 0; i < textAreaInputArray.length; i++) {
+    //debugger;
+    //var eventID = i;
+    var eventInput = textAreaInputsArray[i];
+    console.log(eventInput);
+
+    var saveObj = {
+      //savedID: eventID,
+      text: eventInput,
+    };
+
+    eventArray.push(saveObj);
+
+    //eventText = $(".description").val();
+  }
+  console.log(eventArray);
+
+  localStorage.setItem("events", JSON.stringify(eventArray));
+};
+
+var loadEvent = function () {
+  savedEvent = localStorage.getItem("events");
+
+  if (!savedEvent) {
+    return false;
+  }
+
+  savedEvent = JSON.parse(loadEvents);
+};
+
+//EVENT LISTENERS
+$(".saveBtn").click(saveEvent);
 
 //run auditTime every 5 minutes
 setInterval(auditTime(), 1000 * 60 * 5);
